68c0cbfdeedf7fb4433121feeb9afd723dc82c20
Carlos Lopez <carlosboblopez@gmail.com>
2014-12-09 23:16:24 -0800

    * VPPList Buddy.sln: More debug stuff.
    * VPPList Buddy.userprefs:
    * Debug.fsx:

--- VPPList-Buddy/Resources/Debug.fsx
+++ VPPList-Buddy/Resources/Debug.fsx
@@ -14,8 +14,20 @@ open ExcelLibrary.SpreadSheet
 let assetspath = __SOURCE_DIRECTORY__ 
 let pathgoodvpp = Path.Combine(assetspath,"Sample.xls")
 let pathbadtotals = Path.Combine(assetspath,"Incorrect Totals.xls")
-
+let pathxlstemplate = Path.Combine(assetspath,"Template.xls")
 let basexls = xlsopenfile pathgoodvpp
+
+//Alert Delegates
+let onnonexistentfile = new FileError(fun path -> printfn "Error: File at %s does not exist." path)
+let onemptyworkbook = new FileError(fun path -> printfn "Error: XLS at %s seems to be empty." path)
+let onparsefailure = new FileError(fun path -> printfn "Error: Application could not parse XLS at %s" path)
+let oninvalidvpp = new Alert(fun () -> printfn "Error: XLS is not formatted as VPP Spreadsheet.")
+let onduplicatename = new PartitionError(fun (entry:PartitionEntry) -> printfn "Error: Name %s is repeated." entry.Name)
+let onunderallocation =  new AllocationError(fun num -> printfn "Error: Under-allocated by -%d" num)
+let onoverallocation = new AllocationError( fun num -> printfn "Error: Over-allocated y %d" num)
+let ondestinationerror = new FileError(fun path -> printfn "Error: Directory %s could not be written too." path)
+
+
 
 let randomvpp () = 
     let randomcodeentry = {Code=IO.Path.GetRandomFileName(); URL=IO.Path.GetRandomFileName()}
@@ -30,11 +42,32 @@ let debugvppparser path = //Keep
 
 let openxls path =
     let openxls = new OpenXLSWorkFlow ()
-    openxls.OnNonExistentFile <- new FileError(fun path -> printfn "File %s does not exist!" path)
-    openxls.OnInvalidVPP <- new Alert(fun () -> printfn "VPP not formatted")
+    openxls.OnNonExistentFile <- onnonexistentfile
+    openxls.OnInvalidVPP <- oninvalidvpp
+    openxls.OnParseFailure <- onparsefailure
+    openxls.OnEmptyWorkbook <- onemptyworkbook
+    //openxls.OnInvalidPartitionSheet <- on
 
     match openxls.TryOpenVPP path with
     | true , partitioner -> printfn "good"
     | _ -> printfn "what happened?"
+
+
+let testpartitioner() =
+    let codes = [1..100] |> List.map(fun n -> {Code=n.ToString();URL=sprintf "apple.com/vpp/app/%d.html" n})
+    let vpp = {blankspreadsheet with VPPCodes = codes ; CodesRemaining = 100; CodesPurchased = 100; CodesRedeemed = 0}
+    let pw = PartitionWorkflow(vpp)
+    do 
+        pw.OnDuplicateName.Add(onduplicatename.Invoke)
+        pw.OnOverAllocation.Add(onoverallocation.Invoke)
+        pw.OnUnderAllocation.Add(onunderallocation.Invoke)
+        pw.Add(new PartitionEntry("B128",90))
+        pw.Add(new PartitionEntry("B129",10))
+        pw.Partition(assetspath,fun path -> printfn "Could not write to %s" path)
+
+let xls() =
+    xlsopenfile pathxlstemplate 
+    |> Option.bind(fun xls -> xlsextractsheet xls 0)
+    |> Option.iter(fun template -> savevpptoxlsfromtemplate template (randomvpp()) (Path.Combine(assetspath,"templatetest.xls")))
 
   
