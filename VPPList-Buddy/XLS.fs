1cd7bcefd0f5a4ac54f4861869d3793af875ca25
Carlos Lopez <carlosboblopez@gmail.com>
2014-12-12 19:24:31 -0800

    * XLS.fs: Finish implementing functions with NPOI Library. Now on to   testing!
    * VPPList-Buddy.fsproj: Allow the workflows to compile (and they did!
      Yay composable high order functions!)

--- VPPList-Buddy/XLS.fs
+++ VPPList-Buddy/XLS.fs
@@ -15,9 +15,9 @@ let xlsopenfile path =
     with
     | ex  -> None
 
-let xlstestempty (xls: HSSFWorkbook) = if xls.NumberOfSheets = 0 then None else Some(xls)
+let xlstestempty (xls: IWorkbook) = if xls.NumberOfSheets = 0 then None else Some(xls)
 
-let attachsheet sheet (wb : HSSFWorkbook) =
+let attachsheet sheet (wb : IWorkbook) =
     do wb.Add(sheet)
     wb
 
@@ -43,53 +43,6 @@ let xlstryint ws =
         | (true,i) -> Some(i)
         | (false,_) -> None
     | None -> None
-
-(*
-
-let xlsextractsheet (xls : HSSFWorkbook) sheetindex =
-    match xls.Worksheets.Count , sheetindex with
-    | (0,_) -> None
-    | (_,index) when 0 > index -> None
-    | (sheetcount,index) when sheetcount <= index ->  None
-    | _ -> Some(xls.Worksheets.Item(sheetindex))
-
-let xlsextractsheet' sheetindex xls = xlsextractsheet xls sheetindex
-let xlssaveworksheet (path : string) (ws : Worksheet) =
-    let wb = Workbook()
-    do 
-        wb.Worksheets.Add(ws)
-        wb.Save(path)
-*)
-
-(*
-
-let xlswritecell (sheet : Worksheet) (index : int * int) (obj:Object)  = 
-    do index |> fun (h,v) -> sheet.Cells.Item(h,v) <- new Cell(obj) //How safe is this?
-
-let xlswritetext (sheet : Worksheet) (index : int * int) (text:string) =
-    xlswritecell sheet index text
-
-let xlssaveworkbook (path : string) (wb : Workbook)  = 
-    wb.Save(path)
-    wb
-
-let mapvppdatacell (spreadsheet : VPP) =
-    let vppdata = [
-            (0,0,"Volume Purchase Codes");(0,7,"ProductType");(0,8,spreadsheet.ProductType);(0,9,"|");(0,10,"AdamId:");(0,11,spreadsheet.AdamID);
-            (1,0,"Order ID"); (1,2,spreadsheet.OrderID);
-            (2,0,"Product"); (2,2,spreadsheet.Product);
-            (3,0,"Purchaser"); (3,2,spreadsheet.Purchaser);
-            (4,0,"Codes Purchased"); (4,2,spreadsheet.CodesPurchased |> string);
-            (5,0,"Codes Redeemed"); (5,2,spreadsheet.CodesRedeemed |> string);
-            (6,0,"Codes Remaining"); (6,2,spreadsheet.CodesRemaining |> string);
-            (9,0,"Code");(9,2,"Code Redemption Link")]
-    spreadsheet.VPPCodes //map cells to spreadsheet
-    |> List.mapi(fun vindex code -> 
-        let startindex = 10
-        (vindex + startindex,0,code.Code),(vindex+startindex,2,code.URL))
-    |> List.collect(fun (code,url) -> [code;url])
-    |> List.append vppdata
-
 
 let parsevppspreadsheet cellintreader (cellreader :  int * int -> string option) = //need to verify spreadsheet
     let tryint cell = match cellintreader cell with | Some(int') -> int' | None -> 0
@@ -119,14 +72,58 @@ let parsevppspreadsheet cellintreader (cellreader :  int * int -> string option)
         if anybad then None else Some(vpp) 
     extractinfo blankspreadsheet |> extractcodes |> verify
 
+
+let xlsextractsheet (xls : IWorkbook) sheetindex =
+    match xls.NumberOfSheets , sheetindex with
+    | (0,_) -> None
+    | (_,index) when 0 > index -> None
+    | (sheetcount,index) when sheetcount <= index ->  None
+    | _ -> Some(xls.GetSheetAt(sheetindex))
+
+let xlsextractsheet' sheetindex xls = xlsextractsheet xls sheetindex
+
 let xlsparsevpp worksheet =
     let celltextreader = xlstrystring worksheet
     let cellintreader = xlstryint worksheet
     parsevppspreadsheet cellintreader celltextreader
 
+let mapvppdatacell (spreadsheet : VPP) =
+    let vppdata = [
+            (0,0,"Volume Purchase Codes");(0,7,"ProductType");(0,8,spreadsheet.ProductType);(0,9,"|");(0,10,"AdamId:");(0,11,spreadsheet.AdamID);
+            (1,0,"Order ID"); (1,2,spreadsheet.OrderID);
+            (2,0,"Product"); (2,2,spreadsheet.Product);
+            (3,0,"Purchaser"); (3,2,spreadsheet.Purchaser);
+            (4,0,"Codes Purchased"); (4,2,spreadsheet.CodesPurchased |> string);
+            (5,0,"Codes Redeemed"); (5,2,spreadsheet.CodesRedeemed |> string);
+            (6,0,"Codes Remaining"); (6,2,spreadsheet.CodesRemaining |> string);
+            (9,0,"Code");(9,2,"Code Redemption Link")]
+    spreadsheet.VPPCodes //map cells to spreadsheet
+    |> List.mapi(fun vindex code -> 
+        let startindex = 10
+        (vindex + startindex,0,code.Code),(vindex+startindex,2,code.URL))
+    |> List.collect(fun (code,url) -> [code;url])
+    |> List.append vppdata
+
+(*let xlswritecell (sheet : HSSFSheet) (index : int * int) (obj:Object)  = 
+    //do index |> fun (h,v) -> sheet.Cells.Item(h,v) <- new Cell(obj) //How safe is this?*)
+
+let xlswritetext (sheet : ISheet) (index : int * int) (text:string) =
+    do index |> fun (v,h) -> sheet.CreateRow(v).CreateCell(h).SetCellValue(text)
+
+
+let xlssaveworksheet (path : string) (ws : ISheet) =
+    use fs = File.Create(path)
+    let wb = new HSSFWorkbook()
+    do 
+        wb.Add(ws)
+        wb.Write(fs)
+
+let xlssaveworkbook (path : string) (wb : HSSFWorkbook)  = 
+    use fs = File.Create(path)
+    wb.Write(fs)
+
 let savevpptoxls vpp path =
     let xlsserializer = xlssaveworksheet path
     let write = 
-        fun xlsserializer -> savespreadsheet (Worksheet("Sheet1")) xlswritetext xlsserializer mapvppdatacell
+        fun xlsserializer -> savespreadsheet (xlsworksheet "sheet1") xlswritetext xlsserializer mapvppdatacell
     write xlsserializer vpp
-*)
